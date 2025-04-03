a62af2aba825e03f672cc413271c2b5cd90b68e1
Thiago Araujo Vieira <devthiagoaraujo@gmail.com>
2025-04-02 22:00:08 -0300

    feat: Vehicle's CRUD

--- app/vehicles/urls.py
+++ app/vehicles/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import VehicleViewSet
-from rest_framework.routers import DefaultRouter
-from django.urls import include
-
-router = DefaultRouter()
-router.register(r'vehicle', VehicleViewSet)
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('vehicle/', views.CreateVehicleView.as_view(), name='create_vehicle'),
+    path('vehicle/list/', views.ListVehicleView.as_view(), name='list_vehicle'),
+    path('vehicle/<uuid:pk>/delete/',
+         views.DeleteVehicleView.as_view(), name='delete_vehicle'),
+    path('vehicle/<uuid:pk>/detail/',
+         views.RetrieveVehicleView.as_view(), name='vehicle_detail'),
+    path('vehicle/<uuid:pk>/update/',
+         views.UpdateVehicleView.as_view(), name='update_vehicle'),
 ]
--- app/vehicles/views.py
+++ app/vehicles/views.py
@@ -1,23 +1,38 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from .permissions import IsOwner
+from rest_framework import generics
 from .models import Vehicle
 from .serializers import VehicleSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwner
 
 
-class VehicleViewSet(viewsets.ModelViewSet):
-    queryset = Vehicle.objects.all()
-    serializer_class = VehicleSerializer
+class CreateVehicleView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
-
-    def get_permissions(self):
-        if self.action in ['update', 'partial_update', 'destroy']:
-            return [IsAuthenticated(), IsOwner()]
-        return super().get_permissions()
-    
-    def get_queryset(self):
-        return self.queryset.filter(customer=self.request.user.cliente_profile)
+    serializer_class = VehicleSerializer
+    queryset = Vehicle.objects.all()
 
     def perform_create(self, serializer):
-        print(self.request.user.cliente_profile)
         serializer.save(customer=self.request.user.cliente_profile)
+
+
+class ListVehicleView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = VehicleSerializer
+    queryset = Vehicle.objects.all()
+
+
+class RetrieveVehicleView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+    serializer_class = VehicleSerializer
+    queryset = Vehicle.objects.all()
+
+
+class UpdateVehicleView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+    serializer_class = VehicleSerializer
+    queryset = Vehicle.objects.all()
+
+
+class DeleteVehicleView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+    serializer_class = VehicleSerializer
+    queryset = Vehicle.objects.all()
