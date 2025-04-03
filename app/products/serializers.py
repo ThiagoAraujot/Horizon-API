83ea8d2ea07250da8178f5ae269d15f2a01d371d
Thiago Araujo Vieira <devthiagoaraujo@gmail.com>
2025-04-03 20:51:59 -0300

    refact: Product's serializer to show Fornecedor info

--- app/products/serializers.py
+++ app/products/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Product
+from users.models import FornecedorProfile
+from users.serializers import FornecedorProfileSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    fornecedor = serializers.PrimaryKeyRelatedField(
+        queryset=FornecedorProfile.objects.all(), write_only=True)
+
+    fornecedor_data = FornecedorProfileSerializer(
+        source="fornecedor", read_only=True)
 
     class Meta:
         model = Product
